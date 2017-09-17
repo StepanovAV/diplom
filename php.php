633355470043a207bc9202792778f55b6ec20852
Artem Stepanov <zergatull@gmail.com>
2017-09-17 18:59:01 +0300

    Добавил Php.php, где соединился с бд diplom_db

---
+++ php.php
@@ -0,0 +1,25 @@
+﻿<?php
+	//Настройки соединения с базой
+	$host = "localhost";
+	$base = "diplom_db";
+	$username = "root";
+	$password = "";
+	
+	$mysqli = new mysqli($host, $username, $password, $base);
+
+	/* проверяем соединение */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	/* возвращаем имя текущей базы данных */
+	if ($result = $mysqli->query("SELECT DATABASE()")) {
+		$row = $result->fetch_row();
+		printf("Default database is %s.\n", $row[0]);
+		$result->close();
+	}
+	
+	$mysqli->close();
+
+?>
