93d6284220188e4b86001c7d88f5f6fdc8032cc9
Artem Stepanov <zergatull@gmail.com>
2017-09-17 19:57:44 +0300

    Наконец передал данные с бд через пхп на js что сделало log in рабочим

--- main.php
+++ main.php
@@ -15,28 +15,8 @@
   <input type="submit" id="submit" value="LOGIN"/>
 </div>
 
-<!-- здесь будут что-то... -->
+<!-- Подключаю жс и пхп... -->
 <?php include_once "php.php"?>
-<script>
-	var log = document.getElementById("login");
-	var pass = document.getElementById("password");
-	var submit = document.getElementById("submit");
-	var a = "admin";
-	var b = "1111";
-	console.log(log.value);
-	submit.onclick = function () {
-			if (log.value == a && pass.value == b) {
-				alert("Вы молодец");
-					var exx = document.getElementById("exx");
-					exx.style.display = "";
-					exx.style.color = "green";
-					document.getElementById('loginform').style.display = 'none';
-
-				}
-				else alert ("Идите нахуй! Я вас не знаю!");
-	}
-</script>
-
 
 <div id="exx" style="display: none";>Вы успешно залогинились</div>
 </body>
--- php.php
+++ php.php
@@ -15,11 +15,14 @@
 	
 	echo("<br><br>");
 	
-	if ($result = mysqli_query($mysqli, 'SELECT * FROM login')) { 
+	if ($result = mysqli_query($mysqli, 'SELECT login, password FROM login')) { 
 
     /* Выборка результатов запроса */ 
     while( $row = mysqli_fetch_assoc($result) ){ 
-        printf("%s (%s)\n", $row['login'], $row['password'], $row['email']); 
+        printf("%s (%s)\n", $row['login'], $row['password']); 
+		echo '<script language="javascript">var a = "'.$row['login'].'";</script>';
+		echo '<script language="javascript">var b = "'.$row['password'].'";</script>';
+
     } 
 
     /* Освобождаем используемую память */ 
@@ -30,6 +33,20 @@
 
 
 ?>
-	<script>
-	alert("ЗАЛУПА!!");
-	</script>
+<script>
+	var log = document.getElementById("login");
+	var pass = document.getElementById("password");
+	var submit = document.getElementById("submit");
+	alert(a + "  " + b);
+	submit.onclick = function () {
+			if (log.value == a && pass.value == b) {
+				alert("Вы молодец");
+					var exx = document.getElementById("exx");
+					exx.style.display = "";
+					exx.style.color = "green";
+					document.getElementById('loginform').style.display = 'none';
+
+				}
+				else alert ("Идите нахуй! Я вас не знаю!");
+	}
+</script>
