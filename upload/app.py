b9e3f3fc6b6e46130868b2179b670f35c241f26c
Artem Stepanov <zergatull@gmail.com>
2017-09-29 21:57:47 +0300

    правки кода
    улучшение работы сессий

--- upload/app.py
+++ upload/app.py
@@ -1,4 +1,4 @@
-import os, docx2txt, sqlite3
+import os, docx2txt, sqlite3, re
 from flask import Flask, render_template, request, session, redirect, url_for, g
 
 __author__ = 'artem'
@@ -6,6 +6,61 @@ __author__ = 'artem'
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def getAdmin():
+	conn = sqlite3.connect('login.db')
+	c = conn.cursor()
+	c.execute("SELECT login, password FROM users")
+	admin = c.fetchall()
+	conn.commit()
+	c.close()
+	conn.close()
+	return admin
+	
+def uploadToDb(text):
+	conn = sqlite3.connect('login.db')
+	c = conn.cursor()
+	c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT, password TEXT)")
+	c.execute("CREATE TABLE IF NOT EXISTS labs(id INTEGER PRIMARY KEY AUTOINCREMENT, lab_text TEXT)")
+
+	c.execute(f"INSERT INTO labs(content) VALUES ('{text}')")
+	conn.commit()
+	c.close()
+	conn.close()
+
+def replaceAll(text):
+	text = text.lower();
+	all = ['_','>','<','/','|','\\','+','=',':',';','.',',','!','?','//','(',')','–','-','\n']
+	for i in all:
+		text = text.replace( i , ' ')
+	
+	text = re.sub(r'\s+', ' ', text)
+	return text
+		
+
+def normalize():
+	text = docx2txt.process('../upload/downloads/file.docx')
+	text = replaceAll(text)
+	
+	result = []
+	for char in text:
+		if char.isalnum():
+			result.append(char)
+		elif char.isspace() and (not result or not result[-1].isspace()):
+			result.append(' ')
+			result.append(char)
+	
+	text = ''.join(result)
+	text = text.split(' ')
+
+	result = []
+	for i in text:
+		if len(i) > 1:
+			result.append(i)
+	
+	text = ' '.join(result)
+	
+	return text
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
@@ -15,17 +70,14 @@ def index():
 	if request.method == 'POST':
 		session.pop('user', None)
 		
-		conn = sqlite3.connect('login.db')
-		c = conn.cursor()
-		c.execute("SELECT login, password FROM users")
-		you = c.fetchall()
-		conn.commit()
-		c.close()
-		conn.close()
+		admin = getAdmin()
 		
-		if request.form['password'] == you[0][1] and request.form['username'] == you[0][0]:
+		if request.form['username'] == 'joke':
+			return render_template('joke.html')
+			
+		if request.form['password'] == admin[0][1] and request.form['username'] == admin[0][0]:
 			session['user'] = request.form['username']
-			return redirect(url_for('upload'))
+			return render_template('upload.html')
 
 
 	return render_template('login.html')	
@@ -53,84 +105,13 @@ def upload():
 			destination = "/".join([target, filename])
 			print(destination)
 			file.save(destination)
-			text = docx2txt.process('C:/xampp/htdocs/Diplom/upload/downloads/file.docx')
-			text = text.lower();
-			text = text.replace('{', ' ')
-			text = text.replace('}', ' ')
-			text = text.replace('[', ' ')
-			text = text.replace(']', ' ')
-			text = text.replace('\n',' ')
-			text = text.replace(",", ' ')
-			text = text.replace(".", ' ')
-			text = text.replace("(", ' ')
-			text = text.replace(")", ' ')
-			text = text.replace(';', ' ')
-			text = text.replace(":", ' ')
-			text = text.replace('\"', '')
-			text = text.replace("-", ' ')
-			text = text.replace("_", ' ')
-			text = text.replace("'", '')
-			text = text.replace("%", '')
-			text = text.replace(" а ", ' ')
-			text = text.replace(" у ", ' ')
-			text = text.replace(" і ", ' ')
-			text = text.replace(" в ", ' ')
-			text = text.replace(" з ", ' ')
-			text = text.replace(" ж ", ' ')
-			text = text.replace(" є ", ' ')
-			text = text.replace(' 1 ', ' ')
-			text = text.replace(' 2 ', ' ')
-			text = text.replace(' 3 ', ' ')
-			text = text.replace(' 4 ', ' ')
-			text = text.replace(' 5 ', ' ')
-			text = text.replace(' 6 ', ' ')
-			text = text.replace(' 7 ', ' ')
-			text = text.replace(' 8 ', ' ')
-			text = text.replace(' 9 ', ' ')
-			text = text.replace(' 0 ', ' ')
-			text = text.replace("		", ' ')
-			text = text.replace("	", ' ')
-			text = text.replace("  ", ' ')
-			text = text.replace("  ", ' ')
-			text = text.replace("  ", ' ')
-			text = text.split(' ')
+					
+			text = normalize()
+			uploadToDb(text)
 			
-			buf = 0
-			text2 =[]
-			while buf < len(text):
-				if len(text[buf]) > 2:
-					text2.append(text[buf])
-				buf +=1
-			buf = 0
-			text = ''
-			while buf < len(text2):
-				text+= " "+ text2[buf]
-				buf+=1
-
-			f = open('word.txt', 'w')
-			f.write(text)
-			f.close()
-			print("file uploaded!")
-			
-			conn = sqlite3.connect('login.db')
-			c = conn.cursor()
-			
-			def create_table():
-					c.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, login TEXT, password TEXT)")
-					c.execute("CREATE TABLE IF NOT EXISTS labs(id INTEGER PRIMARY KEY AUTOINCREMENT, lab_text TEXT)")
-
-			def data_entry():
-					c.execute(f"INSERT INTO labs(content) VALUES ('{text}')")
-					conn.commit()
-					c.close()
-					conn.close()
-				
-			create_table()
-			data_entry()
-		
 			return render_template("complete.html", text = text)	
 
 	return redirect(url_for('index'))
 	
 if __name__ == '__main__':
-	app.run(port=7777, debug=True)
+	app.run(port=6317, debug=True)
